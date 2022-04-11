449b7b388710e9a877583205c0f1246f04c3d9da
Vlodkow <vlodkow@gmail.com>
2022-04-11 09:46:14 +0300

    Update transactions

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,17 +2,14 @@ require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
-const METER_TESTNET_PRIVATE_KEY = process.env.METER_TESTNET_PRIVATE_KEY;
-const METER_MAINNET_PRIVATE_KEY = process.env.METER_MAINNET_PRIVATE_KEY;
-
-console.log(METER_TESTNET_PRIVATE_KEY);
+// console.log(process.env.METER_TESTNET_PRIVATE_KEY);
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -27,13 +24,13 @@ module.exports = {
   },
   networks: {
     hardhat: {},
-    meter_testnet: {
-      url: "https://rpctest.meter.io",
-      accounts: [`${METER_TESTNET_PRIVATE_KEY}`]
-    },
-    meter_mainnet: {
-      url: "https://rpc.meter.io",
-      accounts: [`${METER_MAINNET_PRIVATE_KEY}`]
-    }
+    // meter_testnet: {
+    //   url: "https://rpctest.meter.io",
+    //   accounts: [`${process.env.METER_TESTNET_PRIVATE_KEY}`]
+    // },
+    // meter_mainnet: {
+    //   url: "https://rpc.meter.io",
+    //   accounts: [`${process.env.METER_MAINNET_PRIVATE_KEY}`]
+    // }
   },
 };
