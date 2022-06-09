601c9c3e20560020e5a5a8b06c4bea7ce1758d29
vlodkow <vlodkow@gmail.com>
2022-06-09 11:45:06 +0300

    Add networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,11 +9,10 @@ require("@nomiclabs/hardhat-waffle");
 //   }
 // });
 
-// console.log(process.env.METER_TESTNET_PRIVATE_KEY);
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
   solidity: {
     version: "0.8.12",
@@ -37,37 +36,33 @@ module.exports = {
       //   interval: 1000
       // }
     },
-    // meter_testnet: {
-    //   url: "https://rpctest.meter.io",
-    //   accounts: [`${process.env.METER_TESTNET_PRIVATE_KEY}`]
-    // },
-    // meter_mainnet: {
-    //   url: "https://rpc.meter.io",
-    //   accounts: [`${process.env.METER_MAINNET_PRIVATE_KEY}`]
-    // },
-    // bsc_testnet: {
-    //   url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-    //   chainId: 97,
-    //   gasPrice: 20000000000,
-    //   accounts: {mnemonic: ${process.env.BSC_TESTNET_PRIVATE_KEY}}
-    // },
-    // bsc_mainnet: {
-    //   url: "https://bsc-dataseed.binance.org/",
-    //   chainId: 56,
-    //   gasPrice: 20000000000,
-    //   accounts: {mnemonic: ${process.env.BSC_MAINNET_PRIVATE_KEY}}
-    // },
-    // kardiachain_testnet: {
-    //   url: "https://dev.kardiachain.io",
-    //   accounts: [`${process.env.KARDIA_TESTNET_PRIVATE_KEY}`]
-    // },
-    // kardiachain_mainnet: {
-    //   url: "https://rpc.kardiachain.io",
-    //   accounts: [`${process.env.KARDIA_MAINNET_PRIVATE_KEY}`]
-    // },
+    meter_testnet: {
+      url: "https://rpctest.meter.io",
+      accounts: process.env.METER_TESTNET_PRIVATE_KEY !== undefined ? [process.env.METER_TESTNET_PRIVATE_KEY] : [],
+    },
+    meter_mainnet: {
+      url: "https://rpc.meter.io",
+      accounts: process.env.METER_MAINNET_PRIVATE_KEY !== undefined ? [process.env.METER_MAINNET_PRIVATE_KEY] : [],
+    },
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: process.env.BSC_TESTNET_PRIVATE_KEY || "" }
+    },
+    bsc_mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: process.env.BSC_MAINNET_PRIVATE_KEY || "" }
+    },
     emerald_testnet: {
       url: "https://testnet.emerald.oasis.dev",
       accounts: process.env.OASIS_TESTNET_PRIVATE_KEY !== undefined ? [process.env.OASIS_TESTNET_PRIVATE_KEY] : [],
+    },
+    emerald_mainnet: {
+      url: "https://emerald.oasis.dev",
+      accounts: process.env.OASIS_MAINNET_PRIVATE_KEY !== undefined ? [process.env.OASIS_MAINNET_PRIVATE_KEY] : [],
     },
   },
 };
