4ac9354839f4633240ef81af25e521ec040df3a3
vlodkow <vlodkow@gmail.com>
2022-06-15 11:33:01 +0300

    Update

--- src/backend/scripts/upgrade.js
+++ src/backend/scripts/upgrade.js
@@ -1,29 +1,73 @@
 const { ethers, upgrades } = require("hardhat");
 const { saveFrontendFiles } = require('./utils');
 
+const MAIN_PROXY = "";
+const LAND_PROXY = "";
 const ZOMBIE_PROXY = "";
-const MONSTER_PROXY = "0x4ea0Be853219be8C9cE27200Bdeee36881612FF2";
-const MONSTER_HELPER_PROXY = "0x46d4674578a2daBbD0CEAB0500c6c7867999db34";
-const ZOMBIE_HELPER_PROXY = "0xC6c5Ab5039373b0CBa7d0116d9ba7fb9831C3f42";
+const ZOMBIE_HELPER_PROXY = "";
+const MONSTER_PROXY = "";
+const MONSTER_HELPER_PROXY = "";
+const COLLECTION_PROXY = "";
+const TOKEN_PROXY = "";
+const MARKET_PROXY = "";
+
 
 async function main() {
-  // const ZombieV2 = await ethers.getContractFactory("ZombieNFTContract");
-  // const zombieNFT = await upgrades.upgradeProxy(ZOMBIE_PROXY, ZombieV2);
-  // console.log('Contract upgraded');
+  const [deployer, account1] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const MainV2 = await ethers.getContractFactory("MainContract");
+  const main = await upgrades.upgradeProxy(MAIN_PROXY, MainV2);
+
+  const LandV2 = await ethers.getContractFactory("LandNFTContract");
+  const landNFT = await upgrades.upgradeProxy(LAND_PROXY, LandV2);
+
+  const ZombieV2 = await ethers.getContractFactory("ZombieNFTContract");
+  const zombieNFT = await upgrades.upgradeProxy(ZOMBIE_PROXY, ZombieV2);
+
+  const ZombieHelperV2 = await ethers.getContractFactory("ZombieNFTHelperContract");
+  const zombieHelperNFT = await upgrades.upgradeProxy(ZOMBIE_HELPER_PROXY, ZombieHelperV2);
 
   const MonsterV2 = await ethers.getContractFactory("MonsterNFTContract");
   const monsterNFT = await upgrades.upgradeProxy(MONSTER_PROXY, MonsterV2);
 
+  const CollectionV2 = await ethers.getContractFactory("CollectionContract");
+  const collection = await upgrades.upgradeProxy(COLLECTION_PROXY, CollectionV2);
+
+  const TokenV2 = await ethers.getContractFactory("TokenFTContract");
+  const token = await upgrades.upgradeProxy(TOKEN_PROXY, TokenV2);
+
+  const MarketV2 = await ethers.getContractFactory("MarketContract");
+  const market = await upgrades.upgradeProxy(MARKET_PROXY, MarketV2);
+
   const MonsterHelperV2 = await ethers.getContractFactory("MonsterNFTHelperContract");
   const monsterHelperNFT = await upgrades.upgradeProxy(MONSTER_HELPER_PROXY, MonsterHelperV2);
 
-  const ZombieHelperV2 = await ethers.getContractFactory("ZombieNFTHelperContract");
-  const zombieHelperNFT = await upgrades.upgradeProxy(ZOMBIE_HELPER_PROXY, ZombieHelperV2);
   console.log('Contract upgraded');
 
-  saveFrontendFiles(monsterNFT, "MonsterNFTContract");
-  saveFrontendFiles(monsterHelperNFT, "MonsterNFTHelperContract");
+  // const MainContract = await ethers.getContractAt("MainContract", MAIN_PROXY);
+  // await MainContract.updateContractAddress(
+  //   LAND_PROXY,
+  //   ZOMBIE_PROXY,
+  //   ZOMBIE_HELPER_PROXY,
+  //   MONSTER_PROXY,
+  //   MONSTER_HELPER_PROXY,
+  //   TOKEN_PROXY,
+  //   COLLECTION_PROXY,
+  //   MARKET_PROXY,
+  // );
+
+  saveFrontendFiles(main, "MainContract");
+  saveFrontendFiles(landNFT, "LandNFTContract");
+  saveFrontendFiles(zombieNFT, "ZombieNFTContract");
   saveFrontendFiles(zombieHelperNFT, "ZombieNFTHelperContract");
+  saveFrontendFiles(monsterNFT, "MonsterNFTContract");
+  saveFrontendFiles(collection, "CollectionContract");
+  saveFrontendFiles(token, "TokenFTContract");
+  saveFrontendFiles(market, "MarketContract");
+  saveFrontendFiles(monsterHelperNFT, "MonsterNFTHelperContract");
 }
 
 main()
